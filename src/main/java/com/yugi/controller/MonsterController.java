801b6a12a373adbe22fd502bcd6d81d9ba159a1c
Cesarwth <cesarwth@gmail.com>
2023-09-25 12:07:43 -0500

    Update to have a template based on thymeleaf

--- src/main/java/com/yugi/controller/MonsterController.java
+++ src/main/java/com/yugi/controller/MonsterController.java
@@ -4,6 +4,7 @@ import com.yugi.dto.MonsterDTO;
 import com.yugi.service.MonsterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
